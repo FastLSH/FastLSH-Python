5da5c6f6ae849e714335b8776d62bacf1a27115a
PeterYAOHAIXU <yaohaixupeter@gmail.com>
2017-04-21 19:10:13 +0800

    add documentation

--- cmake-build-debug/CMakeFiles/paraGen.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for paraGen.
-# This may be replaced when dependencies are built.
--- cmake-build-debug/lib/googletest/googlemock/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/peter/FYP/FastLSH-Python/lib/googletest/googletest
-# Build directory: /home/peter/FYP/FastLSH-Python/cmake-build-debug/lib/googletest/googlemock/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
